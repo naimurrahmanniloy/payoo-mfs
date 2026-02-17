3b27b3763269b335552087271bc6a0677617e057
Naimur Rahman Niloy <niloyrahman432@gmail.com>
2026-02-17 23:59:09 +0600

    done adding home

---
+++ script/login.js
@@ -0,0 +1,18 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+  //get the mobile number
+  const numberInput = document.getElementById("input-num");
+  const contactNumber = numberInput.value;
+  //get the pin
+  const pinInput = document.getElementById("input-pin");
+  const pin = pinInput.value;
+  // match pin and phone
+  if (contactNumber == "01234567890" && pin == "1234") {
+    alert("hurray");
+    window.location.assign("./home.html");
+  }
+  // if true return alert and  hompage
+  else {
+    alert("Login Failed");
+    return;
+  }
+});
