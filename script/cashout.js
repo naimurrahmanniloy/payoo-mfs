ddb481b46d6fbcb37843e2044b2d3a6f85286b3a
Naimur Rahman Niloy <niloyrahman432@gmail.com>
2026-02-20 23:39:23 +0600

    added function

--- script/cashout.js
+++ script/cashout.js
@@ -1,28 +1,19 @@
 document.getElementById("cashout-btn").addEventListener("click", function () {
-  const cashoutNumberInput = document.getElementById("cashout-num");
-  const cashoutNumber = cashoutNumberInput.value;
-  if (cashoutNumber !== "01234567890") {
+  const cashoutNumber = getValueFromInput("cashout-num");
+  if (cashoutNumber.length != 11) {
     alert("Invalid Mobile Number");
     return;
   }
-
-  const cashoutAmountInput = document.getElementById("cashout-amount");
-  const cashoutAmount = cashoutAmountInput.value;
-  console.log(cashoutAmount);
-
+  const cashoutAmount = getValueFromInput("cashout-amount");
   const balanceElement = document.getElementById("balance");
   const balance = balanceElement.innerText;
-  console.log(balance);
-
   const newBalance = Number(balance) - Number(cashoutAmount);
-  console.log("new balance: ", newBalance);
   if (newBalance < 0) {
     alert("Insufficient Balance");
     return;
   }
 
-  const cashoutPinInput = document.getElementById("cashout-pin");
-  const pin = cashoutPinInput.value;
+  const pin = getValueFromInput("cashout-pin");
   if (pin === "1234") {
     alert("Cashout Successful");
     balanceElement.innerText = newBalance;
---
+++ script/machine.js
@@ -0,0 +1,6 @@
+//machine id -> input value
+function getValueFromInput(id) {
+  const input = document.getElementById(id);
+  const value = input.value;
+  return value;
+}
